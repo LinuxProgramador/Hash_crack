cc16fb3a9f55ce6af59f227e92f3c6056f8e3594
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-08-11 12:26:54 -0600

    dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -3,10 +3,8 @@
 
 system=$( uname -m  | cut -f1)
 
-chmod u+x   ~/Hash_crack/crunch.sh  ~/Hash_crack/dependencies.sh  ~/Hash_crack/crunch  ~/Hash_crack/Hasher.py
-
 function main {
-
+    chmod u+x   ~/Hash_crack/crunch.sh  ~/Hash_crack/dependencies.sh  ~/Hash_crack/crunch  ~/Hash_crack/Hasher.py
     echo -e "\033[1;34minstalando dependencias"
     echo -e "\033[1;37m"
     sleep 2
@@ -17,12 +15,12 @@ function main {
     if [ "$system" != 'aarch64' ] ; then
            apt install crunch -y
            echo $(clear)
-           echo -e "\033[1;34¡listo! ya todo esta configurado"
+           echo -e "\033[1;34¡Listo! ya todo esta configurado"
            echo -e "\033[1;37m"
 
     else
         cp -f /data/data/com.termux/files/home/Hash_crack/crunch  $PATH/
-        echo -e "\033[1;34mel sistema es compatible con el crunch en el directorio Hash_crack"
+        echo -e "\033[1;34mEl sistema es compatible con el crunch del directorio Hash_crack"
         echo -e "\033[1;37m"
     fi
 
