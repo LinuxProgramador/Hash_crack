9a4fb976bbd3d7b4c57071dd872e0078a6494ee5
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-08-12 12:26:21 -0600

    Hasher.py

--- Hasher.py
+++ Hasher.py
@@ -207,18 +207,19 @@ Help Menu:
  ----------------------------
 |  list of available hashes  |
  ----------------------------
-|md5     |
-|sha1    |
-|sha224  |
-|sha256  |
-|sha384  |
-|sha512  |
-|sha3_256|
-|sha3_224|
-|sha3_384|
-|sha3_512|
-|blake2s |
-|blake2b |
+|md5       |
+|sha1      |
+|sha224    |
+|sha256    |
+|sha384    |
+|sha512    |
+|sha3_256  |
+|sha3_224  |
+|sha3_384  |
+|sha3_512  |
+|blake2s   |
+|blake2b   |
+|RIPEMD-160|
  --------
                     """)
     
