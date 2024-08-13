500b087446b80d49bab179a0959943a29fa2371f
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-08-13 11:23:24 -0600

    dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 os=$(uname -o | cut -f1)
-system=$( uname -m  | cut -f1)
+arch=$( uname -m  | cut -f1)
 
 function main {
     chmod u+x   ~/Hash_crack/crunch.sh  ~/Hash_crack/dependencies.sh  ~/Hash_crack/crunch  ~/Hash_crack/Hasher.py
     echo -e "\033[1;34minstalling dependencies"
     echo -e "\033[1;37m"
     sleep 2
-    if [ "$os" = '] ; Then
+    if [ "$os" = 'Android' ] ; then
        apt install python3 python-pip -y
        pip install pyfiglet requests termcolor rarfile colorama pycryptodome
-        echo $(clear)
-
-    if [ "$system" != 'aarch64' ] ; then
+       echo $(clear)
+    elif [ "$os" = 'GNU/Linux' ] ; then
+       
+         
+    if [ "$arch" != 'aarch64' ] ; then
            apt install crunch -y
            echo $(clear)
            echo -e "\033[1;34¡Ready! Everything is now set up"
