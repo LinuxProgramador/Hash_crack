85c91eb875ba526be33b5a7766e6ea7980567d97
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-08-13 18:00:37 -0600

    RARNinja.py

--- RARNinja.py
+++ RARNinja.py
@@ -83,8 +83,8 @@ if __name__ == "__main__":
       
             RAR = input("\nEnter Rute RAR file here: ")
             user=getuser()
-            syst=os.system('uname -o | cut -f1')
-            if syst == 'Android':
+            syst=os.path.exists("/data/data/com.termux/files/")
+            if syst == True:
                 output="/data/data/com.termux/files/home/Hash_crack/wordlist.txt"
             else:
                  output=f"/home/{user}/Hash_crack/wordlist.txt"
