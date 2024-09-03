44ea2aae3d1c77c9a72c5becdbe3e54af1421417
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-09-03 10:19:27 -0600

    crunch.sh

--- crunch.sh
+++ crunch.sh
@@ -1,14 +1,9 @@
 echo $(clear)
 echo "Enter your three words for the concatenated dictionary"
-
 read -p "Enter your first word: " palabra1
-
 read -p "Please enter your second word: " palabra2
-
 read -p "Please enter your third word: " palabra3
 echo $(clear)
-
-
 crunch 1 1 -o ~/Hash_crack/wordlist.txt -p $palabra1 $palabra2 $palabra3
 sleep 2
 echo $(clear)
