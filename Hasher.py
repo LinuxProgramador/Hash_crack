86ee433e7b28686a6ba8be7932883e705496e34e
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-09-11 12:57:32 -0600

    Hasher.py

--- Hasher.py
+++ Hasher.py
@@ -254,21 +254,27 @@ Wait, this may take a while
     if len(hash_input) == self.md5:
              hash_verification="md5"
              print("Type hash => md5")
+             self.cracking_selection(hash_verification,hash_input)
     elif len(hash_input) == self.sha1:
              hash_verification="sha1"
              print("Type hash => (sha1/rypemd-160)")
+             self.cracking_selection(hash_verification,hash_input)
     elif len(hash_input) == self.sha224:
              hash_verification="sha224"
-             print("Type hash => (sha224/sha3_224)")            
+             print("Type hash => (sha224/sha3_224)")   
+             self.cracking_selection(hash_verification,hash_input)
     elif len(hash_input) == self.sha384:
              hash_verification="sha384"
-             print("Type hash => (sha384/sha3_384)")            
+             print("Type hash => (sha384/sha3_384)")   
+             self.cracking_selection(hash_verification,hash_input)
     elif len(hash_input) == self.sha256:
              hash_verification="sha256"
-             print("Type hash => (sha256/sha3_256/blake2s)")           
+             print("Type hash => (sha256/sha3_256/blake2s)")   
+             self.cracking_selection(hash_verification,hash_input)
     elif len(hash_input) == self.sha512:
              hash_verification="sha512"
-             print("Type hash => (sha512/sha3_512/blake2b)")            
+             print("Type hash => (sha512/sha3_512/blake2b)")    
+             self.cracking_selection(hash_verification,hash_input)
     else:
         print()
         self.show_help()
