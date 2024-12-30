4f67adfbae984e72593479385afbd25a7cf26978
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2024-12-29 21:50:32 -0600

    Hasher.py

--- Hasher.py
+++ Hasher.py
@@ -362,7 +362,7 @@ Wait, this may take a while
          self.auxiliary_main(hash_input,hash)
     elif "-sk2" in argv:
          hash_input=input("Enter the hash shake-256: ")
-         if path.isalnum(hash_input): 
+         if hash_input.isalnum(): 
            hash = "shake-256"
            self.auxiliary_main(hash_input,hash)
          else:
