06b36410dfb69e5da6c37d16b136459d67d814ae
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2024-12-31 18:19:19 -0600

    Hasher.py

--- Hasher.py
+++ Hasher.py
@@ -4,7 +4,7 @@
 
 
 from Crypto.Hash import RIPEMD160
-from hashlib import md5,sha1,sha224,sha384,sha256,sha512,sha3_256,sha3_224,sha3_384,sha3_512,blake2s,blake2b,shake_128,shake_256
+from hashlib import md5,sha1,sha224,sha384,sha256,sha512,sha3_256,sha3_224,sha3_384,sha3_512,blake2s,blake2b,shake_128,shake_256,pbkdf2_hmac
 from sys import argv,exit
 from os import system,path,remove
 from time import sleep
