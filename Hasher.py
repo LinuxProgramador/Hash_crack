bab29df51a3dfa81bb09f59306f38b33638ab527
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2025-01-09 20:24:40 -0600

    Hasher.py

--- Hasher.py
+++ Hasher.py
@@ -547,7 +547,7 @@ Wait, this may take a while
           #It's a slow hash
           print("NOTE: The procedure is slow because the hash is obtained with \"PBKDF2\"")
           print("NOTE: It is recommended to use small dictionaries")
-          hash_input = input("Enter the WPA hash: ").strip()
+          hash_input = input("Enter the WPA hash: ").strip().lower()
           ssid = input("Enter the SSID: ").strip()
           if hash_input and ssid:
             if len(hash_input) == 64:
