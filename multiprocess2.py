01088ebdbd66c90119735ea873e18289189543d3
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2025-01-12 14:36:00 -0600

    multiprocess2.py

--- multiprocess2.py
+++ multiprocess2.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
         rute2 = input("Enter the path of the second dictionary: ").strip()
         hash_objetivo = input("Enter the hash to be decrypted: ").strip()
         print("Supported hashes:\n- bcrypt\n- sha512crypt\n- sha256crypt\n- wpa-psk")
-        select = input("Enter the hash type: ").strip()
+        select = input("Enter the hash type: ").strip().lower()
         ssid = input("Enter the SSID (if WPA-PSK): ") if select == "wpa-psk" else None
         wait_time = input("You want to avoid overheating the processor (y/n): ").strip().lower()
     except KeyboardInterrupt:
