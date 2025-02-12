465b27f593b5fa70c60cdaf0350b6540f9b2ad52
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2025-02-12 14:02:12 -0600

    multiprocess1.py

--- multiprocess1.py
+++ multiprocess1.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import multiprocessing
 from Crypto.Hash import RIPEMD160,MD4
 from hashlib import md5, sha1, sha224, sha384, sha256, sha512, sha3_256, sha3_224, sha3_384, sha3_512, blake2s, blake2b, shake_128, shake_256
