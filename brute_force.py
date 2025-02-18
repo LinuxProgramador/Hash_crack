ece4b8b3d9d92d53e03178994ffe741ed8486d17
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2025-02-18 16:18:05 -0600

    brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -221,9 +221,9 @@ def main(hashes, count):
     global is_fast_mode
 
     try:
+        print("INFO:\"bcrypt/shacrypt/wpa-psk/ripemd-160/ntlm\" hashes tend to take longer to decrypt.\n")
         is_fast_mode = input("Do you want to use the fast crack version (y/n): ").strip().lower()
         wait_time = input("Do you want to prevent overheating the processor? (y/n): ").strip().lower()
-        print("\"bcrypt/shacrypt/wpa-psk/ripemd-160/ntlm\" hashes tend to take longer to decrypt")
         hash_input = input("Enter the hash to decrypt: ").strip()
 
         if len(hash_input) == hashes['length_md5']:
