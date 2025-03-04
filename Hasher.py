732bc179c0defb11be6098ddb35da7b6c83790b5
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2025-03-04 14:34:53 -0600

    Hasher.py

--- Hasher.py
+++ Hasher.py
@@ -329,6 +329,16 @@ WARNING:BE CAREFUL WITH THE NUMBER OF PASSWORDS YOU USE. CAN BE GENERATED, IT CA
              else:
                 self.faster(is_fast_mode,crackTimeEstimate,password)
 
+
+          #checking md5crypt hash
+          #It's a slow hash
+          elif select == "md5crypt":
+              if md5_crypt.verify(password, hash_input):
+                 self.auxiliary_crack(password,wpa_psk,ssid)
+              else:
+                self.faster(is_fast_mode,crackTimeEstimate,password)
+
+
           #bcrypt hash check
           #It's a slow hash
           elif select == "bcrypt":
