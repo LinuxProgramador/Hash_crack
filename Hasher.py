e80f0cff2ab06605e6a62a186d9fa9a4140599b4
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2025-03-06 10:21:36 -0600

    Hasher.py

--- Hasher.py
+++ Hasher.py
@@ -451,8 +451,8 @@ WARNING:BE CAREFUL WITH THE NUMBER OF PASSWORDS YOU USE. CAN BE GENERATED, IT CA
        print("Hasher 1.0. Tool for cracking multiple hashes.")
        print("""
 Usage:
-     python3 Hasher.py
-     python3 Hasher.py -ct7 
+     python3 Hasher.py       start the main environment and lead to hash decryption
+     python3 Hasher.py -ct7  parameter to enter a Cisco type 7 encoded key
      python3 Hasher.py -sk   parameter to enter the shake-128 hash
      python3 Hasher.py -sk2  parameter to enter the shake-256 hash
      python3 Hasher.py -wpk  parameter to enter the wpa-psk hash
