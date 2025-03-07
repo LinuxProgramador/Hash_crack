9c5b26a3b82f31102cccb2318fdf31791b5224f1
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2025-03-07 09:36:43 -0600

    multiprocess2.py

--- multiprocess2.py
+++ multiprocess2.py
@@ -19,12 +19,8 @@ def get_encoder():
     print("INFO: For compatibility reasons with certain symbols, choose encoder:")
     print("1) latin-1\n2) utf-8")
     encoder_text = input("option: ")
-    if encoder_text == "1":
-        return "latin-1"
-    elif encoder_text == "2":
-        return "utf-8"
-    else:
-        return "latin-1"
+    select_encoder = "latin-1" if encoder_text == "1" else "utf-8"
+    return select_encoder
 
 def crack(target_hash, word, select, ssid, found, queue, encoder):
     if select == "bcrypt":
