db73b5b9a6e0aa4beab7a47117bfd7f5f485a6b4
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2025-03-18 10:04:56 -0600

    brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -6,7 +6,7 @@ from itertools import product
 from sys import exit
 from Crypto.Hash import RIPEMD160, MD4
 from hashlib import md5, sha1, sha224, sha384, sha256, sha512, sha3_256, sha3_224, sha3_384, sha3_512, blake2s, blake2b, shake_128, shake_256
-from passlib.hash import sha256_crypt, sha512_crypt, md5_crypt, apr_md5_crypt, msdcc2
+from passlib.hash import sha256_crypt, sha512_crypt, md5_crypt, apr_md5_crypt, msdcc2, phpass
 from bcrypt import checkpw
 from hashlib import pbkdf2_hmac,algorithms_available,new
 from base64 import b64decode
