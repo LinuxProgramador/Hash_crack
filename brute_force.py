e689974223105b80b85e863ac802e590e64336dc
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2025-03-18 10:06:28 -0600

    brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -29,6 +29,7 @@ hashes = {
     'md5crypt': md5_crypt,
     'apr1': apr_md5_crypt,
     'DCC2': msdcc2,
+    'phpass':phpass,
     'length_bcrypt': 60,
     'length_md5': 32,
     'length_sha1': 40,
