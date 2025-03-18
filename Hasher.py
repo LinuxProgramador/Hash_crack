f492b4c7ea8cc14e2b37590780619de2ef5be964
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2025-03-18 09:39:28 -0600

    Hasher.py

--- Hasher.py
+++ Hasher.py
@@ -366,7 +366,7 @@ WARNING:BE CAREFUL WITH THE NUMBER OF PASSWORDS YOU USE. CAN BE GENERATED, IT CA
 
           #checking shacrypt,md5crypt,phpass and apr1 hashes
           #It's a slow hash
-          elif select in ["sha256crypt","sha512crypt","md5crypt","apr1"]:
+          elif select in ["sha256crypt","sha512crypt","md5crypt","apr1","phpass"]:
              crackTimeEstimate = 'time unknown'
              if self.hash[select].verify(password, hash_input):
                 self.auxiliary_crack(password,wpa_psk,ssid)
