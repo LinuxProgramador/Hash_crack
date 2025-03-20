4120ba4662920655bae19b0f8ca9ba9d33769521
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2025-03-19 19:35:53 -0600

    Hasher.py

--- Hasher.py
+++ Hasher.py
@@ -236,7 +236,7 @@ WARNING:BE CAREFUL WITH THE NUMBER OF PASSWORDS YOU USE. CAN BE GENERATED, IT CA
       wait_time = input("Do you want to prevent overheating the processor? (y/n): ").strip().lower()
       #Basic rules such as uppercase and lowercase are applied to increase the probability of finding the correct password.
       print("Rules:\n1) Use numbers\n2) Use uppercase letters\n3) Use lowercase letters\n4) Use symbols\n5) Use capital letters only on the first letter\n6) for \"none\"")
-      print("Unsupported combinations (14/23/35/25/)")
+      print("INFO: unsupported combinations (14/23/35/25/)")
       self.rules = input("option: ").strip().replace(" ", "")
       return combined,is_fast_mode,wait_time
     
