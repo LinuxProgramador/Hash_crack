b6a800349963eb9e4f8d68cfa4a64becad49c61a
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2025-03-25 20:08:02 -0600

    multiprocess1.py

--- multiprocess1.py
+++ multiprocess1.py
@@ -45,6 +45,9 @@ def crack(target_hash, word, select, event, queue):
        hash_bytes = sha1(password_bytes).digest()
        second_hash_encoding = sha1(hash_bytes).hexdigest().upper()
        generated_hash =  "*" + second_hash_encoding
+    elif select == "whirlpool":
+       wp = new(bytes(password,encoding=encoder))
+       generated_hash = wp.hexdigest()
     elif select == "sm3":
        supported_hash = [x for x in algorithms_available if x == 'sm3']
        if supported_hash:
