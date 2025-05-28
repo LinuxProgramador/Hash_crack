94198048ba75c710cc2161ceb50c60193f8a2218
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2025-05-28 16:24:55 -0600

    brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -354,6 +354,8 @@ def main(count):
              cracking_selection(count, hash_input, "MySQL 5.X", wait_time, "","")
         elif "$P$" in hash_input[0:3]:
              cracking_selection(count, hash_input, "phpass", wait_time, "","")
+        elif "$argon2id$" in hash_input[0:11]:
+             cracking_selection(count, hash_input, "argon2id", wait_time, "","")
         else:
             if hash_input:
                 consultation = input("The entered hash can be \"shake-128 - shake-256\" (y/n): ").strip().lower()
